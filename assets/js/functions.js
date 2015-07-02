3152046dd6803d5704bab48bfc77d502735586b0
Jesse <Jesse@rescomp-14-284342.stanford.edu>
2015-07-02 00:49:17 -0700

    Finished animations for projects section

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -29,7 +29,7 @@ function workBelt() {
                  
     $('.work-return').click(function() {
         $('.work-belt').css('left','0%');
-        $('.work-container').hide(800);
+        $('.work-container').hide();
     });
                             
 }
